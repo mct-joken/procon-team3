5d7392aec618f8509ff025553a19732f96a98023
kiharu3112 <kijiharu3112@outlook.jp>
2024-06-19 02:15:52 +0900

    chore:ビルドした時に、成果物をdocsにコピーするように

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { resolve } from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/procon-team2/',
+  base: '/procon-team3/',
   build: {
     rollupOptions: {
       input: {
